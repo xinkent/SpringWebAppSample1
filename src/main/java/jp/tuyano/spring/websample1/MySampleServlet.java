8ab9fd849fed6748b148d99fb7dead2439246a20
xinkent <xinkent1994@gmail.com>
2021-03-26 17:49:22 +0900

    add autowire

--- src/main/java/jp/tuyano/spring/websample1/MySampleServlet.java
+++ src/main/java/jp/tuyano/spring/websample1/MySampleServlet.java
@@ -2,15 +2,14 @@ package jp.tuyano.spring.websample1;
 
 import java.io.IOException;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 /**
  * Servlet implementation class MySampleServlet
@@ -19,23 +18,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MySampleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	ApplicationContext app;
+	@Autowired
+	private MyBean mybean1;
 
 	@Override
 	public void init() throws ServletException {
 		super.init();
-		app = new ClassPathXmlApplicationContext("/spring/application-config.xml");
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		MyBean mybean1 = (MyBean)app.getBean("mybean1");
 		request.setAttribute("mybean", mybean1);
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String message = request.getParameter("message");
-		MyBean mybean1 = (MyBean)app.getBean("mybean1");
 		mybean1.addMessage(message);
 		response.sendRedirect("sample");
 	}
